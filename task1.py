d66a236a491bb0d56245ab382356f4f01a8ac0b9
Eugene Aslanov <aslanov.ev@gmail.com>
2016-10-15 14:15:21 +0400

    Add check for first task

--- task1.py
+++ task1.py
@@ -1,15 +1,22 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def f(µ, x):
     return µ * x * (1 - x)
 
 
-µs = [1, 2.8, 3.2, 3.58, 3.583, 4]
+µs = [1., 2.8, 3.2, 3.58, 3.583, 4.]
 
 for µ in µs:
     x_0 = np.arange(0, 1, 0.01)
-    x = np.empty((100, 100))
+    x = np.empty((101, 100))
     x[0, :] = x_0
-    for i in range(1, 100):
+    for i in range(1, 101):
         x[i, :] = f(µ, x[i - 1, :])
+    x_test = x[100, :]
+    for i in range(100):
+        x_test = f(µ, x_test)
+        if np.allclose(x_test, x[100, :], atol=1.e-4):
+            print("µ = {} Order = {}".format(µ, i + 1))
+            break
